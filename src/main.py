5825cddd3f63719863c05a0f4b9c37150840c11a
Sansossio <juliosansossio@gmail.com>
2020-05-26 04:58:44 +0200

    Implement endpoints

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from services import get_value, set_value
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Heroku!'
+    return 'Hello world!'
+
+@app.route('/set')
+def set():
+  key = request.args.get('key')
+  value = request.args.get('value')
+  set_value(key, value)
+  return 'OK'
+
+@app.route('/get')
+def get():
+  key = request.args.get('key')
+  return get_value(key)
 
 if __name__ == '__main__':
     port = os.getenv('PORT') or 80

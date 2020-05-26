da46141b5d9320901fb3b42ebfdd2090cc81cd48
Sansossio <juliosansossio@gmail.com>
2020-05-26 04:48:10 +0200

    First flask routes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Heroku!'
+
+if __name__ == '__main__':
+    port = os.getenv('PORT') or 80
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )

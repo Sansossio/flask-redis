737aaf5d42104cac5c10e25fef423a40c88d9817
Sansossio <juliosansossio@gmail.com>
2020-05-27 11:18:11 +0200

    Value as variable

--- src/services/redis.py
+++ src/services/redis.py
@@ -1,6 +1,8 @@
 import redis
 import os
 
+DEFAULT_VALUE = "Value not found"
+
 host = os.getenv("REDIS_HOST") or "localhost"
 port = os.getenv("REDIS_PORT") or 6379
 r = redis.Redis(host, port)
@@ -9,4 +11,4 @@ def set_value(key: str, value):
   r.set(key, value)
 
 def get_value(key: str):
-  return r.get(key) or "Value not found"
+  return r.get(key) or DEFAULT_VALUE
